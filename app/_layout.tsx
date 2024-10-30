ef2116d83ab2b566ac15705480e11503c43f2194
Andrew Aliaj <andyaliaj@gmail.com>
2024-10-30 13:16:28 -0400

    first commit

---
+++ app/_layout.tsx
@@ -0,0 +1,9 @@
+import { Stack } from "expo-router";
+import "../app/global.css"
+export default function RootLayout() {
+  return (
+    <Stack>
+      <Stack.Screen name="index" />
+    </Stack>
+  );
+}
---
+++ app/index.tsx
@@ -0,0 +1,9 @@
+import { Text, View } from "react-native";
+
+export default function Index() {
+  return (
+    <View className="p-4 bg-black" >
+    <Text className="text-yellow-300">Edit app/index.tsx to edit this screen.</Text>
+  </View>
+  );
+}
