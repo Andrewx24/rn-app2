a0bc95bde6ffe781134b3e107b1a1ab04d415c4b
Andrew Aliaj <andyaliaj@gmail.com>
2024-11-05 12:37:44 -0500

    updates

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,11 +1,26 @@
+import { ClerkProvider, ClerkLoaded } from '@clerk/clerk-expo'
+
 import { Stack } from "expo-router";
 import "../app/global.css";
 
+const publishableKey = process.env.EXPO_PUBLIC_CLERK_PUBLISHABLE_KEY!
+
+if (!publishableKey) {
+  throw new Error(
+    'Missing Publishable Key. Please set EXPO_PUBLIC_CLERK_PUBLISHABLE_KEY in your .env',
+  )
+}
+
 export default function RootLayout() {
+
   return (
+    <ClerkProvider publishableKey={publishableKey}>
+      <ClerkLoaded>
     <Stack>
  
       <Stack.Screen name="(tabs)" options={{ headerShown:false }} />
     </Stack>
+    </ClerkLoaded>
+    </ClerkProvider>
   );
 }
--- lib/types.ts
+++ lib/types.ts
@@ -5,3 +5,9 @@
     email: string;
 }
 
+
+export interface TokenCache {
+    getToken: (key: string) => Promise<string | undefined | null>
+    saveToken: (key: string, token: string) => Promise<void>
+    clearToken?: (key: string) => void
+  }
